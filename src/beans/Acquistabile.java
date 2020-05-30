76e3402477ec7ae36c5927b5018bfe469b007ef5
Merolx <44025793+Merolx@users.noreply.github.com>
2020-05-30 15:11:02 +0200

    bean Acquistabile.java

--- src/beans/Acquistabile.java
+++ src/beans/Acquistabile.java
@@ -1,4 +1,25 @@
 package beans;
 
-public class Acquistabile {
+public interface Acquistabile {
+
+
+    public int getCodice();
+
+
+    public void setCodice(int codice);
+
+    public String getNome();
+
+    public void setNome(String nome);
+
+    public double getPrezzo();
+
+    public void setPrezzo(double prezzo);
+
+    public String getComponenti();
+
+    public void setComponenti(String componenti);
+
+
+
 }
